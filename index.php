e2354326d661871d9b93ee6dba420ddec2137bb3
Gael Mukala <gaelmukala14@gmail.com>
2025-05-02 11:13:08 +0200

    second commit

--- index.php
+++ index.php
@@ -41,12 +41,36 @@ require 'models/articles-data.php';
 
         <div>
 
-            <h3 class="text-4xl font-extrabold dark:text-white">prix des articles </h3>
+            <h3 class="mb-6 text-4xl font-extrabold dark:text-white">Les prix de tous les articles </h3>
+
+            <ul class="max-w-md divide-y divide-gray-200 dark:divide-gray-700">
+                <?php foreach ($articles_prix as $name => $prix): ?>
+                    <li class="pb-3 sm:pb-4">
+                        <div class="flex items-center space-x-4 rtl:space-x-reverse">
+                            <div class="shrink-0">
+                                <img class="w-8 h-8 rounded-full object-cover object-center" src="./assets/images/book.png"
+                                    alt=<?php $name; ?>>
+                            </div>
+                            <div class="flex-1 min-w-0">
+                                <p class="text-sm font-medium text-gray-900 truncate dark:text-white">
+                                    <?php echo $name; ?>
+                                </p>
+                                <p class="text-sm text-gray-500 truncate dark:text-gray-400">
+                                    Ccontact@tpc.com
+                                </p>
+                            </div>
+                            <div class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
+                                <?php echo $prix . 'FC'; ?>
+                            </div>
+                        </div>
+                    </li>
+                <?php endforeach; ?>
+            </ul>
+
             <ul>
                 <!-- affichage de prix des articles  -->
-                <?php foreach ($articles_prix as $prix): ?>
-                    <li><?php echo $prix . 'FC'; ?></li>
-                <?php endforeach; ?>
+
+                <li><?php echo $prix . 'FC'; ?></li>
             </ul>
         </div>
 
@@ -64,16 +88,36 @@ require 'models/articles-data.php';
         <div>
 
 
-            <h3 class="flex items-center text-4xl font-extrabold dark:text-white">Le nom et prix des articles de moin de <span
+            <h3 class="my-6 flex items-center text-4xl font-extrabold dark:text-white">Le nom et prix des articles de
+                moin de
+                <span
                     class="bg-blue-100 text-blue-800 text-2xl font-semibold me-2 px-2.5 py-0.5 rounded-sm dark:bg-blue-200 dark:text-blue-800 ms-2">10000fc</span>
             </h3>
 
 
-            <ul>
+            <ul class="max-w-md divide-y divide-gray-200 dark:divide-gray-700">
                 <!-- affichage de nom des article de moin de 10000FC -->
                 <?php foreach ($articles_prix as $name => $prix): ?>
                     <?php if ($prix < 10000): ?>
-                        <li><?php echo $name . ' : ' . $prix . 'FC'; ?></li>
+                        <li class="pb-3 sm:pb-4">
+                            <div class="flex items-center space-x-4 rtl:space-x-reverse">
+                                <div class="shrink-0">
+                                    <img class="w-8 h-8 rounded-full object-cover object-center" src="./assets/images/logo.png"
+                                        alt=<?php $name; ?>>
+                                </div>
+                                <div class="flex-1 min-w-0">
+                                    <p class="text-sm font-medium text-gray-900 truncate dark:text-white">
+                                        <?php echo $name; ?>
+                                    </p>
+                                    <p class="text-sm text-gray-500 truncate dark:text-gray-400">
+                                        contact@tpc.com
+                                    </p>
+                                </div>
+                                <div class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
+                                    <?php echo $prix . 'FC'; ?>
+                                </div>
+                            </div>
+                        </li>
                     <?php endif; ?>
                 <?php endforeach; ?>
             </ul>
--- produits.php
+++ produits.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 $title = "Produits";
 $header = "Decouvrez tout nos produits";
 ?>
@@ -9,7 +9,35 @@ $header = "Decouvrez tout nos produits";
 <!-- importation des données -->
 <?php require 'models/produits-data.php'; ?>
 
-<h1>Tous les produits</h1>
+<h1 class="mb-6 text-4xl font-extrabold dark:text-white">Tous les produits</h1>
+
+<div class="grid grid-cols-1 md:grid-cols-2 gap-4 mx-auto">
+    <?php foreach ($produits as $produit): ?>
+        <div
+            class="flex flex-col items-center my-3 bg-white border border-gray-200 rounded-lg shadow-sm md:flex-row md:max-w-2xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+            <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg"
+                src="./assets/images/book.png" alt="">
+            <div class="flex flex-col justify-between p-4 leading-normal">
+                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                    <?php echo $produit['nom']; ?></h5>
+                    <span class="text-xl font-bold text-gray-900 dark:text-white">
+                    <?php echo $produit['prix'] . ' ' . $produit['devise']; ?>
+                    </span>
+                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                    <?php foreach ($etudiants as $etudiant): ?>
+                        <?php if ($etudiant['id'] === $produit['etudiant_id']): ?>
+                            Etudiant : <?php echo $etudiant['nom']; ?><br>
+                            Promotion : <?php echo $etudiant['promotion']; ?><br>
+                            Telephone : <?php echo $etudiant['tel']; ?><br>
+                        <?php endif; ?>
+                    <?php endforeach; ?>
+                </p>
+            </div>
+        </div>
+    <?php endforeach; ?>
+</div>
+
+
 
 <div>
     <!-- boucle pour parcourir les produits -->
